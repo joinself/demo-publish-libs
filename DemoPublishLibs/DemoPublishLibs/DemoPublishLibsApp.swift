6dcd290577be5f0cbde8b85cd78b7fbb64aaa976
Long Pham <philong.bmt@gmail.com>
2023-10-23 14:30:10 +0700

    init demo project

---
+++ DemoPublishLibs/DemoPublishLibs/DemoPublishLibsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoPublishLibsApp.swift
+//  DemoPublishLibs
+//
+//  Created by Long Pham on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct DemoPublishLibsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
