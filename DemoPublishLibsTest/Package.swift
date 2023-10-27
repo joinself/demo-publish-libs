591fca6cb0a5e731192c0a42f1fe8d94b1f65703
Long Pham <philong.bmt@gmail.com>
2023-10-27 12:07:46 +0700

    test binary target

--- DemoPublishLibsTest/Package.swift
+++ DemoPublishLibsTest/Package.swift
@@ -11,13 +11,21 @@ let package = Package(
             name: "DemoPublishLibsTest",
             targets: ["DemoPublishLibsTest"]),
     ],
+    dependencies: [
+//        .package(url: "https://github.com/appwrite/sdk-for-swift", from: "0.1.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "DemoPublishLibsTest"),
-        .testTarget(
-            name: "DemoPublishLibsTestTests",
-            dependencies: ["DemoPublishLibsTest"]),
+//        .testTarget(
+//            name: "DemoPublishLibsTestTests",
+//            dependencies: ["DemoPublishLibsTest"]),
+            .binaryTarget(
+                        name: "self_ios_sdk",
+                        url: "https://github.com/joinself/demo-publish-libs/releases/download/0.1/self_ios_sdk.xcframework.zip",
+                        checksum: "094ecfdeacfbd88b1bdcd09390719dd87cab25588efe2abfe2347f3c436bbf60"
+                    )
     ]
 )
